e748d01d5d0952ce0e4e56f286f0d5469e1191df
nafiz Alam <nafizalam.dev@gmail.com>
2026-05-01 05:02:05 +0600

    Show Dynamic News

--- src/app/(main)/news/[id]/page.jsx
+++ src/app/(main)/news/[id]/page.jsx
@@ -1,6 +1,8 @@
 import RightSideBar from "@/components/homepage/news/RightSideBar";
 import { getNewsDetailsById } from "@/lib/data";
 import Image from "next/image";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
 
 const NewsDetailsPage = async ({ params }) => {
 
@@ -9,7 +11,29 @@ const NewsDetailsPage = async ({ params }) => {
     const news = await getNewsDetailsById(id);
 
     return (
-        
+        <div className="grid grid-cols-12 gap-6">
+            <div className="col-span-9">
+                <h1 className=" mb-5">Dragon News</h1>
+                <div className="border border-[#E7E7E7] p-8 rounded-sm">
+                    <div className="flex h-[411px] items-center justify-center overflow-hidden rounded-sm bg-[#F3F3F3] mb-5">
+                        <Image
+                            className="h-full w-full object-contain"
+                            src={news.thumbnail_url}
+                            alt="News Image"
+                            width={518}
+                            height={411}
+                            sizes="(max-width: 768px) 100vw, 518px"
+                        />
+                    </div>
+                    <h1 className="text-2xl text-[#403F3F] font-bold leading-11 mb-2">{news.title}</h1>
+                    <p className="text-[#706F6F] mb-8">{news.details}</p>
+                    <Link className="inline-flex items-center gap-3 bg-[#D72050] px-6 py-3 text-xl font-medium text-white" href={`/category/${news.category_id}`} > <FaArrowLeft /> All news in this category</Link>
+                </div>
+            </div>
+            <div className="col-span-3">
+                <RightSideBar />
+            </div>
+        </div>
     );
 };
 
