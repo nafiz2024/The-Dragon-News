41a5d0e55852f56def5644856af57040a46ae7c8
nafiz Alam <nafizalam.dev@gmail.com>
2026-05-01 04:24:07 +0600

    Complete Right Side Bar

--- src/components/homepage/news/RightSideBar.jsx
+++ src/components/homepage/news/RightSideBar.jsx
@@ -17,7 +17,38 @@ const RightSideBar = () => {
                     <Link href="" className="btn bg-transparent border-black text-black hover:border-[#2563EB] hover:text-[#2563EB]"><FaGithub /> Login with Github</Link>
                 </div>
             </div>
-            
+            <div className="flex flex-col gap-5 mb-5">
+                <h1 className="text-xl text-[#403F3F] font-semibold">Find us On</h1>
+                <div className="border border-[#E7E7E7] rounded-lg">
+                    <Link href="/" className="flex gap-3 border border-[#E7E7E7] p-4 hover:text-[#2563EB]" >
+                        <div className="w-8 h-8 bg-[#F3F3F3] rounded-full flex justify-center items-center"><FaFacebookF /></div>
+                        <h1 className="text-lg text-[#706F6F] font-medium">Facebook</h1>
+                    </Link>
+                    <Link href="/" className="flex gap-3 border border-[#E7E7E7] p-4 hover:text-[#2563EB]" >
+                        <div className="w-8 h-8 bg-[#F3F3F3] rounded-full flex justify-center items-center"><FaTwitter /></div>
+                        <h1 className="text-lg text-[#706F6F] font-medium">Twitter</h1>
+                    </Link>
+                    <Link href="/" className="flex gap-3 border border-[#E7E7E7] p-4 hover:text-[#2563EB]" >
+                        <div className="w-8 h-8 bg-[#F3F3F3] rounded-full flex justify-center items-center"><GrInstagram /></div>
+                        <h1 className="text-lg text-[#706F6F] font-medium">Instagram</h1>
+                    </Link>
+                </div>
+            </div>
+            <div className="bg-[#F3F3F3] p-4">
+                <h1 className="text-xl text-[#403F3F] font-semibold mb-5">Q-Zone</h1>
+                <div className="flex flex-col justify-center items-center gap-5">
+                    <Image src={swimmingImg} alt="Swimming Image" width={258} height={219} />
+                <Image src={classImg} alt="Swimming Image" width={258} height={219} />
+                <Image src={playgroundImg} alt="Swimming Image" height={219} />
+                </div>
+            </div>
+            <div className="w-full mt-5">
+                <Image
+                    src={bannerImg}
+                    alt="Banner Image"
+                    className="w-full h-auto"
+                />
+            </div>
         </div>
     );
 };
