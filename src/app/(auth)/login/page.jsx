2cdbff3e03745732d46ee93ccc82a124a7d3809e
nafiz Alam <nafizalam.dev@gmail.com>
2026-05-02 02:26:21 +0600

    Install React Hook Form And deploy

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,30 +1,48 @@
-import Link from "next/link";
+'use client'
 
-export const metadata = {
-  title: "The Dragon News | Login",
-  description: "The Grate Online News Portal In Bangladesh",
-  icons: {
-    icon: "/favicon-transparent.png",
-    shortcut: "/favicon-transparent.png",
-    apple: "/favicon-transparent.png",
-  },
-};
+import Link from "next/link";
+import { useForm } from "react-hook-form"
+
+// export const metadata = {
+//   title: "The Dragon News | Login",
+//   description: "The Grate Online News Portal In Bangladesh",
+//   icons: {
+//     icon: "/favicon-transparent.png",
+//     shortcut: "/favicon-transparent.png",
+//     apple: "/favicon-transparent.png",
+//   },
+// };
 
 const LoginPage = () => {
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const handleLogicFunc = (data) => {
+    console.log(data)
+  }
+
   return (
     <div className="flex justify-center items-center">
       <div className="w-[752px] bg-white p-20 rounded-sm mb-14">
         <h1 className="text-center text-4xl font-semibold text-[#403F3F] pb-14 border-b border-[#E7E7E7] mb-14">Login your account</h1>
-        <fieldset className="fieldset px-4">
-          
-          <label className="label text-xl text[#403F3F] font-semibold pb-3">Email</label>
-          <input type="email" className="input w-full h-13 mb-5 bg-[#F3F3F3] border-none p-5 placeholder:text-[16px]" placeholder="Enter your email address" />
+        <form onSubmit={handleSubmit(handleLogicFunc)}>
+          <fieldset className="fieldset px-4">
 
-          <label className="label text-xl text[#403F3F] font-semibold pb-3">Password</label>
-          <input type="password" className="input w-full h-13 bg-[#F3F3F3] border-none p-5 placeholder:text-[16px]" placeholder="Enter your password" />
+            <label className="label text-xl text[#403F3F] font-semibold pb-3">Email</label>
+            <input type="email" className="input w-full h-13 bg-[#F3F3F3] border-none p-5 placeholder:text-[16px]" placeholder="Enter your email address" {...register("email", { required: "Email field is required" })} />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
 
-          <Link href="/" className="btn btn-neutral mt-8 p-6 bg-[#403F3F] text-xl font-semibold">Login</Link>
-        </fieldset>
+            <label className="label text-xl text[#403F3F] font-semibold pb-3 mt-5">Password</label>
+            <input type="password" className="input w-full h-13 bg-[#F3F3F3] border-none p-5 placeholder:text-[16px]" placeholder="Enter your password" {...register("password", { required: "Password field is required" })} />
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+
+            <button type="submit" className="btn btn-neutral mt-8 p-6 bg-[#403F3F] text-xl font-semibold">Login</button>
+          </fieldset>
+        </form>
         <div className="flex gap-2 mt-8 justify-center">
           <h1 className="text-lg font-semibold text-[#706F6F]">Don't Have An Account ? </h1>
           <Link className="text-lg font-semibold text-[#FF8C47]" href="/register">Register</Link>
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,37 +1,61 @@
-import Link from "next/link";
+'use client'
 
-export const metadata = {
-  title: "The Dragon News | Register",
-  description: "The Grate Online News Portal In Bangladesh",
-  icons: {
-    icon: "/favicon-transparent.png",
-    shortcut: "/favicon-transparent.png",
-    apple: "/favicon-transparent.png",
-  },
-};
+import Link from "next/link";
+import { useForm } from "react-hook-form"
+
+// export const metadata = {
+//   title: "The Dragon News | Register",
+//   description: "The Grate Online News Portal In Bangladesh",
+//   icons: {
+//     icon: "/favicon-transparent.png",
+//     shortcut: "/favicon-transparent.png",
+//     apple: "/favicon-transparent.png",
+//   },
+// };
 
 const RegisterPage = () => {
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const handleLogicFunc = (data) => {
+    console.log(data)
+  }
+
   return (
     <div>
       <div className="flex justify-center items-center">
         <div className="w-[752px] bg-white p-20 rounded-sm mb-14">
           <h1 className="text-center text-4xl font-semibold text-[#403F3F] pb-14 border-b border-[#E7E7E7] mb-14">Register your account</h1>
-          <fieldset className="fieldset px-4">
+          <form onSubmit={handleSubmit(handleLogicFunc)}>
+            <fieldset className="fieldset px-4">
 
-            <label className="label text-xl text[#403F3F] font-semibold pb-3">Your Name</label>
-            <input type="text" className="input w-full h-13 mb-5 bg-[#F3F3F3] border-none p-5 placeholder:text-[16px]" placeholder="Enter your name address" />
+              <label className="label text-xl text[#403F3F] font-semibold pb-3">Your Name</label>
+              <input type="text" className="input w-full h-13 bg-[#F3F3F3] border-none p-5 placeholder:text-[16px]" placeholder="Enter your name address" {...register("name", { required: "Name field is required" })} />
+              {errors.name && (
+                <p className="text-red-500">{errors.name.message}</p>
+              )}
 
-            <label className="label text-xl text[#403F3F] font-semibold pb-3">Photo URL</label>
-            <input type="name" className="input w-full h-13 mb-5 bg-[#F3F3F3] border-none p-5 placeholder:text-[16px]" placeholder="Enter your photo url" />
+              <label className="label text-xl text[#403F3F] font-semibold pb-3 mt-5">Photo URL</label>
+              <input type="name" className="input w-full h-13 bg-[#F3F3F3] border-none p-5 placeholder:text-[16px]" placeholder="Enter your photo url" {...register("photo", { required: "Photo field is required" })} />
+              {errors.photo && (
+                <p className="text-red-500">{errors.photo.message}</p>
+              )}
 
-            <label className="label text-xl text[#403F3F] font-semibold pb-3">Email</label>
-            <input type="email" className="input w-full h-13 mb-5 bg-[#F3F3F3] border-none p-5 placeholder:text-[16px]" placeholder="Enter your email address" />
+              <label className="label text-xl text[#403F3F] font-semibold pb-3 mt-5">Email</label>
+              <input type="email" className="input w-full h-13 bg-[#F3F3F3] border-none p-5 placeholder:text-[16px]" placeholder="Enter your email address" {...register("email", { required: "Email field is required" })} />
+              {errors.email && (
+                <p className="text-red-500">{errors.email.message}</p>
+              )}
 
-            <label className="label text-xl text[#403F3F] font-semibold pb-3">Password</label>
-            <input type="password" className="input w-full h-13 bg-[#F3F3F3] border-none p-5 placeholder:text-[16px]" placeholder="Enter your password" />
+              <label className="label text-xl text[#403F3F] font-semibold pb-3 mt-5">Password</label>
+              <input type="password" className="input w-full h-13 bg-[#F3F3F3] border-none p-5 placeholder:text-[16px]" placeholder="Enter your password" {...register("password", { required: "Password field is required" })} />
+              {errors.password && (
+                <p className="text-red-500">{errors.password.message}</p>
+              )}
 
-            <Link href="/" className="btn btn-neutral mt-8 p-6 bg-[#403F3F] text-xl font-semibold">Register</Link>
-          </fieldset>
+              <button type="submit" className="btn btn-neutral mt-8 p-6 bg-[#403F3F] text-xl font-semibold">Register</button>
+            </fieldset>
+          </form>
         </div>
       </div>
     </div>
